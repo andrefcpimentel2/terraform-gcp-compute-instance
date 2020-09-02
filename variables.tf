a79e8b41f040584deea1a04781ce608ce34ad646
andrefcpimentel2 <andre@hashicorp.com>
2020-09-02 14:25:52 +0100

    labels can only be lowercase

--- variables.tf
+++ variables.tf
@@ -19,12 +19,12 @@ default     = "n1-standard-4"
 
 variable "owner" {
 description = "IAM user responsible for lifecycle of cloud resources used for training"
-default = "Terraform"
+default = "terraform"
 }
 
 variable "created-by" {
 description = "Tag used to identify resources created programmatically by Terraform"
-default     = "Terraform"
+default     = "terraform"
 }
 
 variable "sleep-at-night" {
